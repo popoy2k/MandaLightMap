9f37b423bd2e747e5733401bf7103f0c9f83538c
Carl Alingalan <carl.alingalan@gmail.com>
2019-09-10 01:48:34 +0800

    landing page done

--- client/src/components/main/NavBar.js
+++ client/src/components/main/NavBar.js
@@ -31,43 +31,11 @@ export class NavBar extends Component {
                 <a href="#">Statistics</a>
               </li>
               <li>
-                <a href="#" className="active">
-                  About Us
-                </a>
+                <a href="#">About Us</a>
               </li>
             </ul>
           </nav>
         </header>
-        <section className="bg-frst">
-          <h1>Mandaluyong City: Light Pollution </h1>
-          <div className="content-msg">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              mollitia minus reprehenderit earum dignissimos praesentium ullam!
-              Ullam perspiciatis incidunt reprehenderit est exercitationem eius
-              dicta. Alias consequatur aliquid inventore incidunt repellendus
-              dolor voluptates. Laudantium harum atque, maiores voluptatibus
-              saepe aperiam, tenetur minus sapiente doloremque itaque placeat.
-              Fuga quae rerum sit, corporis ex numquam perspiciatis
-              reprehenderit dolorum sequi fugit unde quidem nesciunt ipsa a
-              voluptates. Nobis harum nihil ipsam quae quo fugit numquam modi
-              dolorum necessitatibus odio, molestias in dignissimos voluptate
-              doloremque incidunt culpa placeat ducimus temporibus! Veniam
-              alias, inventore cumque fuga, tenetur modi qui tempore iusto iste
-              deserunt dolores excepturi distinctio.
-            </p>
-          </div>
-        </section>
-        <section className="content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-            nobis suscipit fugit omnis deleniti. Quasi optio ducimus
-            exercitationem dolor ullam suscipit corporis. Provident aspernatur,
-            ratione quasi quo at dolorem unde voluptatibus possimus ipsa hic
-            placeat eveniet sit! Iste eaque voluptates tempora doloribus labore
-            ipsa repudiandae! Tempore esse nobis asperiores voluptatum?
-          </p>
-        </section>
       </Fragment>
     );
   }
--- client/src/components/main/login.js
+++ client/src/components/main/login.js
@@ -4,6 +4,11 @@ import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 
 export class login extends Component {
+  constructor(props) {
+    super(props);
+    document.title = "Skótos - Login";
+  }
+
   state = {
     email: "",
     password: ""
@@ -25,42 +30,82 @@ export class login extends Component {
     return (
       <Fragment>
         <NavBar />
-        <div className="d-flex align-items-center" style={{ height: "100vh" }}>
-          <div className="container">
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 offset-md-3">
-              <div className="card">
-                <div className="card-body">
-                  <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                      <label htmlFor="">Email</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        name="email"
-                        value={email}
-                        autoComplete="off"
-                        onChange={this.onChange}
-                      />
+        <div>
+          <section className="bg-frst">
+            <div className="left-brand">
+              <h1>Skótos</h1>
+              <hr />
+              <div className="left-brand-sub-contianer">
+                <p>
+                  &nbsp;&nbsp; The word "
+                  <strong>
+                    <i>Skótos</i>
+                  </strong>
+                  " is a greek word that means Bright. This website is purposely
+                  made to help researchers, students and other individuals that
+                  seek for "
+                  <strong>
+                    <i>Mandaluyong City</i>
+                  </strong>
+                  " Light Pollution Data Statistics. And also this website
+                  provide articles, researches, study related with light
+                  pollution that can help to their own projects.
+                </p>
+                <button className="csp-btn">Read more...</button>
+              </div>
+            </div>
+          </section>
+          <section className="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laudantium nobis suscipit fugit omnis deleniti. Quasi optio
+              ducimus exercitationem dolor ullam suscipit corporis. Provident
+              aspernatur, ratione quasi quo at dolorem unde voluptatibus
+              possimus ipsa hic placeat eveniet sit! Iste eaque voluptates
+              tempora doloribus labore ipsa repudiandae! Tempore esse nobis
+              asperiores voluptatum?
+            </p>
+          </section>
+          <div
+            className="d-flex align-items-center"
+            style={{ height: "100vh" }}
+          >
+            <div className="container">
+              <div className="col-12 col-sm-12 col-md-6 col-lg-6 offset-md-3">
+                <div className="card">
+                  <div className="card-body">
+                    <form onSubmit={this.onSubmit}>
+                      <div className="form-group">
+                        <label htmlFor="">Email</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          name="email"
+                          value={email}
+                          autoComplete="off"
+                          onChange={this.onChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="">Password</label>
+                        <input
+                          type="password"
+                          name="password"
+                          autoComplete="off"
+                          value={password}
+                          className="form-control"
+                          onChange={this.onChange}
+                        />
+                      </div>
+                      <button className="btn btn-block btn-outline-primary">
+                        Login
+                      </button>
+                    </form>
+                    <div className="d-block justify-content-left pt-2">
+                      <Link to="/register" className="cLink">
+                        Don't have an account yet ?
+                      </Link>
                     </div>
-                    <div className="form-group">
-                      <label htmlFor="">Password</label>
-                      <input
-                        type="password"
-                        name="password"
-                        autoComplete="off"
-                        value={password}
-                        className="form-control"
-                        onChange={this.onChange}
-                      />
-                    </div>
-                    <button className="btn btn-block btn-outline-primary">
-                      Login
-                    </button>
-                  </form>
-                  <div className="d-block justify-content-left pt-2">
-                    <Link to="/register" className="cLink">
-                      Don't have an account yet ?
-                    </Link>
                   </div>
                 </div>
               </div>
